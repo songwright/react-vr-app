507ff4f91ec6043f2b97f6f95f60fbbeddeee524
Ramon Sanchez <songwright66@gmail.com>
2020-03-10 09:57:57 -0700

    Created Penholder component

---
+++ src/Penholder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { asset, View, Model, StyleSheet } from 'react-vr';
+
+export default class Penholder extends React.Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+};
