30862f7f5eb42de457db28fc3f12d13f38faab31
Ramon Sanchez <songwright66@gmail.com>
2020-03-10 10:45:51 -0700

    Added models to Penholder component

--- src/Penholder.js
+++ src/Penholder.js
@@ -5,6 +5,46 @@ export default class Penholder extends React.Component {
   render() {
     return (
       <View>
+        <Model
+          source={{
+            obj: asset('/penholder/Table_Set_obj.obj'),
+            mtl: asset('/penholder/Table_Set_obj.mtl')
+          }}
+          style={{
+            transform: [
+              {translate: [-2.35, 0.65, -4]},
+              { scale: 0.00125 },
+              { rotateX: -90 }
+            ]
+          }}
+        />
+        <Model
+          source={{
+            obj: asset('/penholder/Table_Set_obj.obj'),
+            mtl: asset('/penholder/Table_Set_obj.mtl')
+          }}
+          style={{
+            transform: [
+              {translate: [5.5, 0.65, -4]},
+              { scale: 0.00125 },
+              { rotateX: -90 }
+            ]
+          }}
+        />
+        <Model
+          source={{
+            obj: asset('/penholder/Table_Set_obj.obj'),
+            mtl: asset('/penholder/Table_Set_obj.mtl')
+          }}
+          style={{
+            transform: [
+              {translate: [12, 0.5, -4]},
+              { scale: 0.00125 },
+              { rotateX: -90 },
+              { rotateZ: -270 }
+            ]
+          }}
+        />
       </View>
     );
   }
