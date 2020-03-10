7cd5c81e934e9a68ea4930f86106fc811db2733f
Ramon Sanchez <songwright66@gmail.com>
2020-03-10 13:23:18 -0700

    Added changeScene function for onClick in App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import { asset, Pano, Text, View, AmbientLight } from 'react-vr';
+import { asset, Pano, Text, View, AmbientLight, Sound, VrButton } from 'react-vr';
 // import Primitives from './Primitives';
 import Penholder from './Penholder';
 
 export default class App extends React.Component {
+  changeScene() {
+    console.log("It was clicked.");
+  }
   render() {
     return (
       <View>
-        <AmbientLight intensity={ 2.5 } />
-        <Pano source={asset('background.jpeg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            // layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 2, -6]}],
-          }}>
-          My office
-        </Text>
+        <AmbientLight intensity={2.5} />
+        <Pano source={asset('background.jpeg')} />
+        <Sound
+          loop={true}
+          volume={0.7}
+          source={{
+            wav: asset('office.wav')
+          }}
+        />
+        <VrButton onClick={this.changeScene}>
+          <Text
+            style={{
+              backgroundColor: '#777879',
+              fontSize: 0.8,
+              fontWeight: '400',
+              // layoutOrigin: [0.5, 0.5],
+              paddingLeft: 0.2,
+              paddingRight: 0.2,
+              textAlign: 'center',
+              textAlignVertical: 'center',
+              transform: [{ translate: [0, 2, -6] }],
+            }}>
+            My office
+          </Text>
+        </VrButton>
         <Penholder />
       </View>
     );
