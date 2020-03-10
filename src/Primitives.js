b000358a76de16cd359baf751b4f1b40d44dc56c
Ramon Sanchez <songwright66@gmail.com>
2020-03-09 21:39:09 -0700

    Created Primitives component

---
+++ src/Primitives.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Box, Cylinder, Sphere, StyleSheet } from 'react-vr';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Box
+          dimWidth={2}
+          dimDepth={2}
+          dimHeight={1}
+        />
+      </View>
+    );
+  }
+};
+
+
