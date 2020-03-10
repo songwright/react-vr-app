b58907e28e96b9eb37a7aa367ab1ee930d9755e3
Ramon Sanchez <songwright66@gmail.com>
2020-03-10 13:28:11 -0700

    Added absolute position to models in Penholder

--- src/Penholder.js
+++ src/Penholder.js
@@ -11,6 +11,7 @@ export default class Penholder extends React.Component {
             mtl: asset('/penholder/Table_Set_obj.mtl')
           }}
           style={{
+            position: 'absolute',
             transform: [
               {translate: [-2.35, 0.65, -4]},
               { scale: 0.00125 },
@@ -24,6 +25,7 @@ export default class Penholder extends React.Component {
             mtl: asset('/penholder/Table_Set_obj.mtl')
           }}
           style={{
+            position: 'absolute',
             transform: [
               {translate: [5.5, 0.65, -4]},
               { scale: 0.00125 },
@@ -37,6 +39,7 @@ export default class Penholder extends React.Component {
             mtl: asset('/penholder/Table_Set_obj.mtl')
           }}
           style={{
+            position: 'absolute',
             transform: [
               {translate: [12, 0.5, -4]},
               { scale: 0.00125 },
