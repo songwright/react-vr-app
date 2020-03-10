bf8b22e4f6633b1bdb5603829d0a9704f0c6fd17
Ramon Sanchez <songwright66@gmail.com>
2020-03-10 10:45:25 -0700

    Moved text location in App.js

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ export default class App extends React.Component {
             backgroundColor: '#777879',
             fontSize: 0.8,
             fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
+            // layoutOrigin: [0.5, 0.5],
             paddingLeft: 0.2,
             paddingRight: 0.2,
             textAlign: 'center',
             textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
+            transform: [{translate: [0, 2, -6]}],
           }}>
-          Hello!
+          My office
         </Text>
         <Penholder />
       </View>
